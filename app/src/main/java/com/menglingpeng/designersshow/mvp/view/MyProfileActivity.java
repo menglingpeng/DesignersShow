3796f7164de1e51ddfeef649d69e75c002b74628
menglingpeng <menglingpeng@live.com>
2017-11-30 22:29:28 +0800

    feat: add dialog for logout

--- app/src/main/java/com/menglingpeng/designersshow/mvp/view/MyProfileActivity.java
+++ app/src/main/java/com/menglingpeng/designersshow/mvp/view/MyProfileActivity.java
@@ -1,6 +1,8 @@
 package com.menglingpeng.designersshow.mvp.view;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -63,8 +65,8 @@ private HashMap<String, String> map;
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if(type.equals(Constants.REQUEST_AUTH_USER)){
-            if(item.getItemId() == R.id.profile_login_out){
-                loginOut();
+            if(item.getItemId() == R.id.profile_logout){
+                showLogoutDialog();
             }else {
 
             }
@@ -74,6 +76,27 @@ private HashMap<String, String> map;
             }
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showLogoutDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        AlertDialog dialog;
+        builder.setTitle(getText(R.string.profile_logout));
+        builder.setMessage(getText(R.string.are_you_sure_want_to_logout));
+        builder.setNegativeButton(getText(R.string.cancel), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.setPositiveButton(getText(R.string.profile_logout), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                loginOut();
+            }
+        });
+        dialog = builder.create();
+        dialog.show();
     }
 
     private void loginOut(){
@@ -87,6 +110,7 @@ private HashMap<String, String> map;
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
     }
+
 
     @Override
     public void hideProgress() {
