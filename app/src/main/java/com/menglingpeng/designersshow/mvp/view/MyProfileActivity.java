f732d951ed83a13fe947045da6ba56b4e04eaa23
menglingpeng <menglingpeng@live.com>
2017-11-30 21:46:17 +0800

    feat: add logout function for profile page

--- app/src/main/java/com/menglingpeng/designersshow/mvp/view/MyProfileActivity.java
+++ app/src/main/java/com/menglingpeng/designersshow/mvp/view/MyProfileActivity.java
@@ -1,9 +1,13 @@
 package com.menglingpeng.designersshow.mvp.view;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 
@@ -59,6 +63,56 @@ private HashMap<String, String> map;
     }
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(type.equals(Constants.REQUEST_AUTH_USER)){
+            if(item.getItemId() == R.id.profile_logout){
+                showLogoutDialog();
+            }else {
+
+            }
+        }else {
+            if(item.getItemId() == R.id.profile_share){
+
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showLogoutDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        AlertDialog dialog;
+        builder.setTitle(getText(R.string.profile_logout));
+        builder.setMessage(getText(R.string.are_you_sure_want_to_logout));
+        builder.setNegativeButton(getText(R.string.cancel), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.setPositiveButton(getText(R.string.profile_logout), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                loginOut();
+            }
+        });
+        dialog = builder.create();
+        dialog.show();
+    }
+
+    private void loginOut(){
+        SharedPreUtil.saveState(Constants.IS_LOGIN, false);
+        SharedPreUtil.deleteAuthToken();
+        restartApplication();
+    }
+
+    private void restartApplication(){
+        Intent intent = getPackageManager().getLaunchIntentForPackage(getPackageName());
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+
+    @Override
     public void hideProgress() {
         progressBar.setVisibility(ProgressBar.GONE);
     }
@@ -70,6 +124,6 @@ private HashMap<String, String> map;
 
     @Override
     public void loadSuccess(String json, String requestType) {
-
+        
     }
 }
