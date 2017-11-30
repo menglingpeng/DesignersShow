86949cf5fadd20fb2ce7f8d714dace5ba33d2d19
menglingpeng <menglingpeng@live.com>
2017-11-30 22:11:28 +0800

    feat: add restartApplication function for login out

--- app/src/main/java/com/menglingpeng/designersshow/mvp/view/MyProfileActivity.java
+++ app/src/main/java/com/menglingpeng/designersshow/mvp/view/MyProfileActivity.java
@@ -1,5 +1,6 @@
 package com.menglingpeng.designersshow.mvp.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -78,6 +79,13 @@ private HashMap<String, String> map;
     private void loginOut(){
         SharedPreUtil.saveState(Constants.IS_LOGIN, false);
         SharedPreUtil.deleteAuthToken();
+        restartApplication();
+    }
+
+    private void restartApplication(){
+        Intent intent = getPackageManager().getLaunchIntentForPackage(getPackageName());
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 
     @Override
