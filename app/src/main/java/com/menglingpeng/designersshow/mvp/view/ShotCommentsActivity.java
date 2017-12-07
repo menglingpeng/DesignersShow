eb84305725ac3bd5564331ab1f403dc706267d91
menglingpeng <menglingpeng@live.com>
2017-12-07 22:09:20 +0800

    feat: add layout of add comment function

--- app/src/main/java/com/menglingpeng/designersshow/mvp/view/ShotCommentsActivity.java
+++ app/src/main/java/com/menglingpeng/designersshow/mvp/view/ShotCommentsActivity.java
@@ -1,18 +1,26 @@
 package com.menglingpeng.designersshow.mvp.view;
 
 
+import android.support.design.widget.FloatingActionButton;
 import android.view.View;
 import android.support.v7.widget.Toolbar;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 
 import com.menglingpeng.designersshow.BaseActivity;
 import com.menglingpeng.designersshow.R;
 import com.menglingpeng.designersshow.utils.Constants;
+import com.menglingpeng.designersshow.utils.ImageLoader;
+import com.menglingpeng.designersshow.utils.SharedPreUtil;
 
 public class ShotCommentsActivity extends BaseActivity {
 
     private Toolbar toolbar;
     private String id;
+    private ImageView avatarIv;
+    private EditText editText;
+    private FloatingActionButton floatingActionButton;
     private static RecyclerFragment fragment;
 
     @Override
@@ -24,6 +32,9 @@ public class ShotCommentsActivity extends BaseActivity {
     protected void initViews() {
         super.initViews();
         toolbar = (Toolbar) findViewById(R.id.comments_tb);
+        avatarIv = (ImageView)findViewById(R.id.add_comment_avatar_iv);
+        editText = (EditText)findViewById(R.id.add_comment_et);
+        floatingActionButton = (FloatingActionButton)findViewById(R.id.add_comment_fab);
         toolbar.setTitle(getIntent().getStringExtra(Constants.COMMENTS_COUNT));
         setSupportActionBar(toolbar);
         toolbar.setNavigationIcon(R.drawable.ic_back);
@@ -36,6 +47,7 @@ public class ShotCommentsActivity extends BaseActivity {
         id = getIntent().getStringExtra(Constants.SHOT_ID).toString();
         fragment = RecyclerFragment.newInstance(id, Constants.REQUEST_LIST_COMMENTS);
         replaceFragment(fragment);
+        ImageLoader.loadCricleImage(getApplicationContext(), SharedPreUtil.getLoginData(Constants.AUTH_USER_AVATAR_URL), avatarIv);
     }
 
     public static RecyclerFragment getFragment() {
