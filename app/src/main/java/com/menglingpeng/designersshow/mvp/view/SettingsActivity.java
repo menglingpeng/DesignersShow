645993ecc43bf89610b93aa85d76e565b0d3e9bf
menglingpeng <menglingpeng@live.com>
2017-12-10 19:09:42 +0800

    feat: add layout of SettingsActivity

---
+++ app/src/main/java/com/menglingpeng/designersshow/mvp/view/SettingsActivity.java
@@ -0,0 +1,32 @@
+package com.menglingpeng.designersshow.mvp.view;
+
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.menglingpeng.designersshow.BaseActivity;
+import com.menglingpeng.designersshow.R;
+
+public class SettingsActivity extends BaseActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void initLayoutId() {
+        layoutId = R.layout.activity_settings;
+    }
+
+    @Override
+    protected void initViews() {
+        super.initViews();
+        toolbar = (Toolbar)findViewById(R.id.settings_tb);
+        toolbar.setTitle(getString(R.string.title_activity_settings));
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/menglingpeng/designersshow/mvp/view/SettingsFragment.java
@@ -0,0 +1,8 @@
+package com.menglingpeng.designersshow.mvp.view;
+
+/**
+ * Created by mengdroid on 2017/12/10.
+ */
+
+public class SettingsFragment {
+}
