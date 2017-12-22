0c6b5278f49c459b033ea111d0315ec348044889
menglingpeng <menglingpeng@live.com>
2017-12-22 21:43:12 +0800

    feat: add layout for AboutActivity

---
+++ app/src/main/java/com/menglingpeng/designersshow/mvp/view/AboutActivity.java
@@ -0,0 +1,36 @@
+package com.menglingpeng.designersshow.mvp.view;
+
+
+
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.menglingpeng.designersshow.BaseActivity;
+import com.menglingpeng.designersshow.R;
+
+public class AboutActivity extends BaseActivity {
+
+    private Toolbar toolbar;
+    private CollapsingToolbarLayout collapsingToolbarLayout;
+
+    @Override
+    protected void initLayoutId() {
+        layoutId = R.layout.activity_about;
+    }
+
+    @Override
+    protected void initViews() {
+        super.initViews();
+        collapsingToolbarLayout = (CollapsingToolbarLayout)findViewById(R.id.about_cdl);
+        toolbar = (Toolbar)findViewById(R.id.about_tb);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
