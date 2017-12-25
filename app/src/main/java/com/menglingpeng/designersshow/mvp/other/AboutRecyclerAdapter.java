861608a590dedb635d38b26bd3ebed686c697b2a
menglingpeng <menglingpeng@live.com>
2017-12-25 13:57:53 +0800

    feat: add adapter for list open resource

--- app/src/main/java/com/menglingpeng/designersshow/mvp/other/AboutRecyclerAdapter.java
+++ app/src/main/java/com/menglingpeng/designersshow/mvp/other/AboutRecyclerAdapter.java
@@ -1,10 +1,19 @@
 package com.menglingpeng.designersshow.mvp.other;
 
 import android.content.Context;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.menglingpeng.designersshow.R;
 import com.menglingpeng.designersshow.mvp.interf.OnRecyclerListItemListener;
+import com.menglingpeng.designersshow.mvp.model.OpenResource;
+import com.menglingpeng.designersshow.utils.ShareAndOpenInBrowserUtil;
+
+import java.util.ArrayList;
 
 /**
  * Created by mengdroid on 2017/12/24.
@@ -13,26 +22,57 @@ import com.menglingpeng.designersshow.mvp.interf.OnRecyclerListItemListener;
 public class AboutRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
 
     private Context context;
-    private OnRecyclerListItemListener listener;
-    public AboutRecyclerAdapter (Context context, OnRecyclerListItemListener listener){
+    private ArrayList<OpenResource> list;
+    public AboutRecyclerAdapter (Context context){
         this.context = context;
-        this.listener = listener;
+        list = new ArrayList<>();
     }
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.open_source_licenses_recycler_item, null);
+        RecyclerView.ViewHolder holder = new ViewHolder(view);
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        ViewHolder viewHolder = (ViewHolder)holder;
+        final OpenResource openResource = list.get(position);
+        viewHolder.nameTv.setText(openResource.getName());
+        viewHolder.authorNameTv.setText(openResource.getAuthorName());
+        viewHolder.licenseTv.setText(openResource.getLicense());
+        viewHolder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShareAndOpenInBrowserUtil.openInBrowser(context, openResource.getUrl());
+            }
+        });
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return list.size();
     }
 
-    public
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private final CardView cardView;
+        private final TextView nameTv;
+        private final TextView authorNameTv;
+        private final TextView licenseTv;
+
+        public ViewHolder(View view) {
+            super(view);
+            cardView = (CardView)view.findViewById(R.id.about_open_source_licenses_cv);
+            nameTv = (TextView)view.findViewById(R.id.about_open_source_name_tv);
+            authorNameTv = (TextView)view.findViewById(R.id.about_open_source_author_tv);
+            licenseTv = (TextView)view.findViewById(R.id.about_open_source_licenses_name_tv);
+        }
+    }
+
+    public void addData(OpenResource openResource){
+        list.add(openResource);
+        notifyDataSetChanged();
+    }
 }
--- app/src/main/java/com/menglingpeng/designersshow/mvp/view/AboutActivity.java
+++ app/src/main/java/com/menglingpeng/designersshow/mvp/view/AboutActivity.java
@@ -3,18 +3,27 @@ package com.menglingpeng.designersshow.mvp.view;
 
 
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import com.menglingpeng.designersshow.BaseActivity;
 import com.menglingpeng.designersshow.R;
+import com.menglingpeng.designersshow.mvp.model.OpenResource;
+import com.menglingpeng.designersshow.mvp.other.AboutRecyclerAdapter;
+
+import java.util.ArrayList;
 
 public class AboutActivity extends BaseActivity {
 
     private Toolbar toolbar;
     private CollapsingToolbarLayout collapsingToolbarLayout;
     private RecyclerView recyclerView;
+    private AboutRecyclerAdapter adapter;
+    private LinearLayoutManager linearLayoutManager;
+    private ArrayList<OpenResource> list;
 
     @Override
     protected void initLayoutId() {
@@ -27,6 +36,7 @@ public class AboutActivity extends BaseActivity {
         collapsingToolbarLayout = (CollapsingToolbarLayout)findViewById(R.id.about_cdl);
         toolbar = (Toolbar)findViewById(R.id.about_tb);
         recyclerView = (RecyclerView)findViewById(R.id.about_rv);
+        list = new ArrayList<>();
         setSupportActionBar(toolbar);
         toolbar.setNavigationIcon(R.drawable.ic_back);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -35,5 +45,13 @@ public class AboutActivity extends BaseActivity {
                 finish();
             }
         });
+        linearLayoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL,
+                false);
+        adapter = new AboutRecyclerAdapter(getApplicationContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(adapter);
+        for(int i = 0; i < list.size(); i++){
+            adapter.addData(list.get(i));
+        }
     }
 }
