ee1abf9d3483a98c24e894e5b6c811ca8e10c104
menglingpeng <menglingpeng@live.com>
2018-06-07 22:07:10 +0800

    add the function of list my favorites image for favorites page

---
+++ app/src/main/java/com/menglingpeng/designersshow/mvp/other/FavoritesRecyclerAdapter.java
@@ -0,0 +1,25 @@
+package com.menglingpeng.designersshow.mvp.other;
+
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+
+
+public class FavoritesRecyclerAdapter extends  RecyclerView.Adapter<RecyclerView.ViewHolder>{
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
