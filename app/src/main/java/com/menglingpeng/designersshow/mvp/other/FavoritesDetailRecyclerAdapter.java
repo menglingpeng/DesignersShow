eb5a5e73f9ad564cf4a58e5d776da28f0db705b6
menglingpeng <menglingpeng@live.com>
2018-06-13 22:24:44 +0800

    feat: add progressbar for favorites detail page

--- app/src/main/java/com/menglingpeng/designersshow/mvp/other/FavoritesDetailRecyclerAdapter.java
+++ app/src/main/java/com/menglingpeng/designersshow/mvp/other/FavoritesDetailRecyclerAdapter.java
@@ -67,8 +67,85 @@ public class FavoritesDetailRecyclerAdapter extends RecyclerView.Adapter<Recycle
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        if(holder instanceof FavoritesRecyclerAdapter.BookViewHolder){
-            final FavoritesRecyclerAdapter.BookViewHolder viewHolder = (FavoritesRecyclerAdapter.BookViewHolder)holder;
+        if(holder instanceof BaseDetailViewHolder){
+            final BaseDetailViewHolder viewHolder = (BaseDetailViewHolder)holder;
+            final Shot shot = (Shot) list.get(position);
+            String shotUrl = null;
+            boolean isGif = shot.isAnimated();
+            int attachmentsCount = shot.getAttachments_count();
+            viewHolder.shotTitleTv.setText(shot.getTitle());
+            viewHolder.authorTv.setText(shot.getUser().getName());
+            viewHolder.shotCreatedTimeTv.setText(TimeUtil.getTimeDifference(shot.getUpdated_at()));
+            if(SharedPrefUtil.getState(Constants.SAVING_LOWER_IMAGE)){
+                shotUrl = shot.getImages().getTeaser();
+            }else {
+                shotUrl = shot.getImages().getNormal();
+            }
+            if(SharedPrefUtil.getState(Constants.GIFS_AUTO_PLAY) && isGif){
+                ImageLoader.load(fragment, shotUrl, viewHolder.shotIv, false, true);
+            }else {
+                ImageLoader.load(fragment, shotUrl, viewHolder.shotIv, false, false);
+            }
+            if (isGif) {
+                viewHolder.shotGifIv.setVisibility(ImageView.VISIBLE);
+            }
+            viewHolder.shotLikesCountTv.setText(String.valueOf(shot.getLikes_count()));
+            viewHolder.shotCommentsCountTv.setText(String.valueOf(shot.getComments_count()));
+            viewHolder.shotViewsCountTv.setText(String.valueOf(shot.getViews_count()));
+            if (attachmentsCount != 0) {
+                viewHolder.shotAttachmentsCountIv.setVisibility(ImageView.VISIBLE);
+                viewHolder.shotAttachmentsCountTv.setVisibility(TextView.VISIBLE);
+                viewHolder.shotAttachmentsCountTv.setText(String.valueOf(attachmentsCount));
+            }
+            viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mListener != null) {
+                        mListener.onRecyclerFragmentListListener(viewHolder, shot);
+                    }
+                }
+            });
+        }else if(holder instanceof BookDetailViewHolder){
+            final BookDetailViewHolder viewHolder = (BookDetailViewHolder)holder;
+            final Shot shot = (Shot) list.get(position);
+            String shotUrl = null;
+            boolean isGif = shot.isAnimated();
+            int attachmentsCount = shot.getAttachments_count();
+            viewHolder.shotTitleTv.setText(shot.getTitle());
+            viewHolder.authorTv.setText(shot.getUser().getName());
+            viewHolder.shotCreatedTimeTv.setText(TimeUtil.getTimeDifference(shot.getUpdated_at()));
+            if(SharedPrefUtil.getState(Constants.SAVING_LOWER_IMAGE)){
+                shotUrl = shot.getImages().getTeaser();
+            }else {
+                shotUrl = shot.getImages().getNormal();
+            }
+            if(SharedPrefUtil.getState(Constants.GIFS_AUTO_PLAY) && isGif){
+                ImageLoader.load(fragment, shotUrl, viewHolder.shotIv, false, true);
+            }else {
+                ImageLoader.load(fragment, shotUrl, viewHolder.shotIv, false, false);
+            }
+            if (isGif) {
+                viewHolder.shotGifIv.setVisibility(ImageView.VISIBLE);
+            }
+            viewHolder.shotLikesCountTv.setText(String.valueOf(shot.getLikes_count()));
+            viewHolder.shotCommentsCountTv.setText(String.valueOf(shot.getComments_count()));
+            viewHolder.shotViewsCountTv.setText(String.valueOf(shot.getViews_count()));
+            if (attachmentsCount != 0) {
+                viewHolder.shotAttachmentsCountIv.setVisibility(ImageView.VISIBLE);
+                viewHolder.shotAttachmentsCountTv.setVisibility(TextView.VISIBLE);
+                viewHolder.shotAttachmentsCountTv.setText(String.valueOf(attachmentsCount));
+            }
+            viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mListener != null) {
+                        mListener.onRecyclerFragmentListListener(viewHolder, shot);
+                    }
+                }
+            });
+
+        }else if (holder instanceof ThirdDetailViewHolder){
+            final ThirdDetailViewHolder viewHolder = (ThirdDetailViewHolder)holder;
             final Shot shot = (Shot) list.get(position);
             String shotUrl = null;
             boolean isGif = shot.isAnimated();
--- app/src/main/java/com/menglingpeng/designersshow/mvp/view/UserFavoritesDetailActivity.java
+++ app/src/main/java/com/menglingpeng/designersshow/mvp/view/UserFavoritesDetailActivity.java
@@ -2,12 +2,20 @@ package com.menglingpeng.designersshow.mvp.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.menglingpeng.designersshow.BaseActivity;
 import com.menglingpeng.designersshow.R;
+import com.menglingpeng.designersshow.mvp.interf.OnRecyclerListItemListener;
 
-public class UserFavoritesDetailActivity extends BaseActivity {
+public class UserFavoritesDetailActivity extends BaseActivity implements OnRecyclerListItemListener{
 
+    private Toolbar toolbar;
+    private String type;
+    private String title;
+    private static RecyclerFragment fragment;
 
     @Override
     protected void initLayoutId() {
@@ -17,5 +25,21 @@ public class UserFavoritesDetailActivity extends BaseActivity {
     @Override
     protected void initViews() {
         super.initViews();
+        toolbar = (Toolbar) findViewById(R.id.user_favorites_detail_tb);
+        title = getString(R.string.nav_my_favorites);
+        toolbar.setTitle(title);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public <T> void onRecyclerFragmentListListener(RecyclerView.ViewHolder viewHolder, T t) {
+
     }
 }
